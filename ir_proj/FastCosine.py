b8cdf6c882699619850acee3e9809a493c08cb9a
Amiros1322 <zehaviamir9@gmail.com>
2022-01-10 01:29:50 +0200

    Finished FastCosine, more or less

--- ir_proj/FastCosine.py
+++ ir_proj/FastCosine.py
@@ -5,6 +5,19 @@ for word in query:
 		if doc_id not in scores.keys():
 			scores[doc_id] = 0
 		scores[dic_id] += count
-	# possible optimization: turn scores into a numpy array, keep doc_id-lengths as numpy array, and do a division
-	for doc in in scores.keys():
-		scores[doc] = scores[doc]
+	# possible optimization: turn scores into a numpy array, keep doc_id-lengths as numpy array, and do a division (cant, different length vectors - unless scores is an array)
+	# another optimization: manual doc_id-index mapping.
+
+        # If scores >> 100, use a heap instead of list and sort.
+        for doc in in scores.keys():
+		scores[doc] = scores[doc]/lengths(doc)
+	return sorted(list(scores.items()), key=lambda t: t[1], reverse=True)[:100]
+
+        # I wasnt sure about heapify. More efficient to insert into a heap, but then you cant divide the sums by the length.
+	#if len(scores.keys()) < 200:
+        #        return sorted(list(scores.items()), key=lambda t: t[1], reverse=True)[:100]
+        #else:
+        #        heapq.
+                
+	
+	
