c52a2a2ff02bffaa3e717ce2b64458e55dbeea2d
Amiros1322 <zehaviamir9@gmail.com>
2022-01-12 13:46:17 +0200

    Running ngrok

--- ir_proj/search_frontend.py
+++ ir_proj/search_frontend.py
@@ -210,144 +210,6 @@ class MultiFileReader:
         self.close()
         return False
 
-TUPLE_SIZE = 6  # We're going to pack the doc_id and tf values in this
-# many bytes.
-TF_MASK = 2 ** 16 - 1  # Masking the 16 low bits of an integer
-
-
-class InvertedIndex:
-    def __init__(self, docs={}):
-        """ Initializes the inverted index and add documents to it (if provided).
-        Parameters:
-        -----------
-          docs: dict mapping doc_id to list of tokens
-        """
-        # stores document frequency per term
-        self.df = Counter()
-        # stores total frequency per term
-        self.term_total = Counter()
-        # stores posting list per term while building the index (internally),
-        # otherwise too big to store in memory.
-        self._posting_list = defaultdict(list)
-        # mapping a term to posting file locations, which is a list of
-        # (file_name, offset) pairs. Since posting lists are big we are going to
-        # write them to disk and just save their location in this list. We are
-        # using the MultiFileWriter helper class to write fixed-size files and store
-        # for each term/posting list its list of locations. The offset represents
-        # the number of bytes from the beginning of the file where the posting list
-        # starts.
-        self.posting_locs = defaultdict(list)
-
-        for doc_id, tokens in docs.items():
-            self.add_doc(doc_id, tokens)
-
-    def add_doc(self, doc_id, tokens):
-        """ Adds a document to the index with a given `doc_id` and tokens. It counts
-            the tf of tokens, then update the index (in memory, no storage
-            side-effects).
-        """
-        w2cnt = Counter(tokens)
-        self.term_total.update(w2cnt)
-        for w, cnt in w2cnt.items():
-            self.df[w] = self.df.get(w, 0) + 1
-            self._posting_list[w].append((doc_id, cnt))
-
-    def write_index(self, base_dir, name):
-        """ Write the in-memory index to disk. Results in the file:
-            (1) `name`.pkl containing the global term stats (e.g. df).
-        """
-        self._write_globals(base_dir, name)
-
-    def _write_globals(self, base_dir, name):
-        with open(Path(base_dir) / f'{name}.pkl', 'wb') as f:
-            pickle.dump(self, f)
-
-    def __getstate__(self):
-        """ Modify how the object is pickled by removing the internal posting lists
-            from the object's state dictionary.
-        """
-        state = self.__dict__.copy()
-        del state['_posting_list']
-        return state
-
-    def posting_lists_iter(self):
-        """ A generator that reads one posting list from disk and yields
-            a (word:str, [(doc_id:int, tf:int), ...]) tuple.
-        """
-        with closing(MultiFileReader()) as reader:
-            for w, locs in self.posting_locs.items():
-                #print(f"PostingLocsIter: w:{w}, \nlocs:{locs}") 
-                b = reader.read(locs, self.df[w] * TUPLE_SIZE)
-                posting_list = []
-                for i in range(self.df[w]):
-                    doc_id = int.from_bytes(b[i * TUPLE_SIZE:i * TUPLE_SIZE + 4], 'big')
-                    tf = int.from_bytes(b[i * TUPLE_SIZE + 4:(i + 1) * TUPLE_SIZE], 'big')
-                    posting_list.append((doc_id, tf))
-                yield w, posting_list
-
-    @staticmethod
-    def read_index(base_dir, name):
-        with open(Path(base_dir) / f'{name}.pkl', 'rb') as f:
-            return pickle.load(f)
-
-    @staticmethod
-    def delete_index(base_dir, name):
-        path_globals = Path(base_dir) / f'{name}.pkl'
-        path_globals.unlink()
-        for p in Path(base_dir).rglob(f'{name}_*.bin'):
-            p.unlink()
-
-    @staticmethod
-    def write_a_posting_list(b_w_pl):
-        ''' Takes a (bucket_id, [(w0, posting_list_0), (w1, posting_list_1), ...])
-        and writes it out to disk as files named {bucket_id}_XXX.bin under the
-        current directory. Returns a posting locations dictionary that maps each
-        word to the list of files and offsets that contain its posting list.
-        Parameters:
-        -----------
-          b_w_pl: tuple
-            Containing a bucket id and all (word, posting list) pairs in that bucket
-            (bucket_id, [(w0, posting_list_0), (w1, posting_list_1), ...])
-        Return:
-          posting_locs: dict
-            Posting locations for each of the words written out in this bucket.
-        '''
-        posting_locs = defaultdict(list)
-        bucket, list_w_pl = b_w_pl
-
-        with closing(MultiFileWriter('.', bucket)) as writer:
-            for w, pl in list_w_pl:
-                # convert to bytes
-                b = b''.join([(doc_id << 16 | (tf & TF_MASK)).to_bytes(TUPLE_SIZE, 'big')
-                              for doc_id, tf in pl])
-                # write to file(s)
-                locs = writer.write(b)
-                # save file locations to index
-                posting_locs[w].extend(locs)
-        return posting_locs
-
-"""### Postings list"""
-'''
-fs = gcsfs.GCSFileSystem(project = 'irproject-206655839')
-with fs.open('ir-project_bucket_amirnitzan') as f:
-    posting_locs = pickle.load(f)
-'''
-'''
-bucket_name = 'ir-nitzan_amir_bucket_206655839'
-client = storage.Client('project2-337820')
-blobs = client.list_blobs(bucket_name)
-
-super_posting_locs = defaultdict(list)
-print(client.list_blobs(bucket_name))
-for blob in client.list_blobs(bucket_name, prefix=''):
-  if not blob.name.endswith("pickle"):
-    continue
-  with blob.open("rb") as f:
-    posting_locs = pickle.load(f)
-    for k, v in posting_locs.items():
-        super_posting_locs[k].extend(v)
-'''
-
 #reading and saving the titkle id dict so we cxan connect id to their title
 id_title_pickle = "id_title_dict.pickle"
 with open(id_title_pickle, 'rb') as f:
@@ -706,7 +568,18 @@ TUPLE_SIZE = 6       # We're going to pack the doc_id and tf values in this
                      # many bytes.
 TF_MASK = 2 ** 16 - 1 # Masking the 16 low bits of an integer
 
-DL = {}  # We're going to update and calculate this after each document. This will be usefull for the calculation of AVGDL (utilized in BM25) 
+DL = {}  # We're going to update and calculate this after each document. This will be usefull for the calculation of AVGDL (utilized in BM25)
+def read_posting_list(index, w):
+    with closing(MultiFileReader()) as reader:
+        locs = index.posting_locs[w]
+        b = reader.read(locs, index.df[w] * TUPLE_SIZE)
+        posting_list = []
+        for i in range(index.df[w]):
+            doc_id = int.from_bytes(b[i * TUPLE_SIZE:i * TUPLE_SIZE + 4], 'big')
+            tf = int.from_bytes(b[i * TUPLE_SIZE + 4:(i + 1) * TUPLE_SIZE], 'big')
+            posting_list.append((doc_id, tf))
+    return posting_list
+
 class InvertedIndex:  
   def __init__(self, docs={}):
     """ Initializes the inverted index and add documents to it (if provided).
@@ -812,16 +685,6 @@ class InvertedIndex:
     # save file locations to index
     self.posting_locs[w].extend(locs)
 
-  def read_posting_list(inverted, w):
-    with closing(MultiFileReader()) as reader:
-        locs = inverted.posting_locs[w]
-        b = reader.read(locs, inverted.df[w] * TUPLE_SIZE)
-        posting_list = []
-        for i in range(inverted.df[w]):
-            doc_id = int.from_bytes(b[i * TUPLE_SIZE:i * TUPLE_SIZE + 4], 'big')
-            tf = int.from_bytes(b[i * TUPLE_SIZE + 4:(i + 1) * TUPLE_SIZE], 'big')
-            posting_list.append((doc_id, tf))
-    return posting_list
     # This was the old posting list before we replaced it with the above
   #def read_posting_list(self,w):
   #  with closing(MultiFileReader()) as reader:
@@ -888,19 +751,19 @@ def search():
     '''
     res = []
     query = request.args.get('query', '')
+
     if len(query) == 0:
       return jsonify(res)
     # BEGIN SOLUTION
     query = tokenize(query)
     index = app.inverted
 
-  
     # Save postings lists to memory, calculate dfs
     word_postings = dict()
     word_df = dict()
     for Qword in query:
-        word_postings[Qword] = index.read_posting_list(Qword)
-        word_df[Qword]= len(word_postings[Qword])
+        word_postings[Qword] = read_posting_list(index, Qword)
+        word_df[Qword] = len(word_postings[Qword])
 
     # Calculate unique doc_ids
     ids = []
@@ -981,15 +844,6 @@ def search_body():
 
     text_index = InvertedIndex(docs=doc_test)
     '''
-    query = tokenize(query)
-    index = inverted
-    words, p_lst = index.read_posting_list()
-    #nizan### changed from index to index 2
-    doc_tfidf_mat = generate_document_tfidf_matrix(query, index, words, p_lst)
-    query_tfidf = generate_query_tfidf_vector(query, index)
-    cos_sim_dict = cosine_similarity(doc_tfidf_mat, query_tfidf)
-    top_n = get_top_n(cos_sim_dict, 100)
-
     # END SOLUTION
     return jsonify(res)
 
